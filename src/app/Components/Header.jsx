3dffd0089ffa6cb4df34173bbcfab69c002eca23
Abderrahmanlahmidi <Abderrahmanlahmidi@github.com>
2024-04-21 15:27:24 +0100

    feat: add the resume link

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -10,7 +10,7 @@ const header = () => {
                     <h1 className="font-[600] text-[40px] tracking-[-0.6px] text-[#ffff] max-[640px]:text-[32px] " >Abderrahmane Lahmidi</h1>
                     <h2 className="text-[#d5e8ff] leading-[40px] font-[600] text-[32px] max-[640px]:text-[24px] " >Front-End Developer</h2>
                     <p className="text-[20px] max-[640px]:text-[18px] text-[#DADDE7] font-[300]" >Passionate and motivated front-end developer with a strong desire to
-                       excel in creating visually appealing and user-friendly web applications.</p>
+                       excel in creating visually appealing and user-friendly web applications See my <a className="underline text-[20px] max-[640px]:text-[16px] font-[500] text-[#BEDFFE]" href="https://drive.google.com/file/d/1sLPclIBZHqTLqding1NsANPGJwQYYiwB/view?usp=drive_link">Resume</a> </p>
                 </div>
             </div>
          </div>
